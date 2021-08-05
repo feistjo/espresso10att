f7e8cbd514750018385dc9c090db2474b8a2b723
feistjo <40469822+feistjo@users.noreply.github.com>
2021-08-05 13:43:45 -0500

    Update lineage.mk

--- espresso10att/lineage.mk
+++ espresso10att/lineage.mk
@@ -25,7 +25,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 #DEVICE_PACKAGE_OVERLAYS += device/samsung/espressowifi/overlay/custom-common
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/espressowifi/aosp_espressowifi.mk)
+#$(call inherit-product, device/samsung/espressowifi/aosp_espressowifi.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_espresso10att
